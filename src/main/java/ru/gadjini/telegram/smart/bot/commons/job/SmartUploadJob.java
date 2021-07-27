f74dae0210263f492a4267749a74021a20dae051
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-07-27 18:28:32 +0300

    Джоба умных загрузок должна работать только на основном сервере

--- src/main/java/ru/gadjini/telegram/smart/bot/commons/job/SmartUploadJob.java
+++ src/main/java/ru/gadjini/telegram/smart/bot/commons/job/SmartUploadJob.java
@@ -1,11 +1,14 @@
 package ru.gadjini.telegram.smart.bot.commons.job;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import ru.gadjini.telegram.smart.bot.commons.common.Profiles;
 import ru.gadjini.telegram.smart.bot.commons.service.queue.UploadQueueService;
 
 @Component
+@Profile({Profiles.PROFILE_DEV_PRIMARY, Profiles.PROFILE_PROD_PRIMARY})
 public class SmartUploadJob {
 
     private static final int EXPIRATION_TIME_IN_SECONDS = 30 * 60;
